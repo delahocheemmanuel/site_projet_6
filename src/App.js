5eb1fb343640032413f2913cce51f426281da8bf
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-07-03 15:58:14 +0200

    Route par défaut pour toutes les adresses non spécifiées

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import About from "./assets/Pages/About";
 import Error from "./assets/Pages/Error";
 import Accommodation from "./assets/Pages/Accommodation";
 
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/error" element={<Error />} />
-      <Route exact path="/Accommodation/:cardId" element={<Accommodation />} />
+      <Route exact path="/accommodation/:cardId" element={<Accommodation />} />
 
+      {/* Route par défaut pour toutes les adresses non spécifiées */}
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
 
 export default App;
-
