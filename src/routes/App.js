dbdc3784a583f372feeb0f58b5fa8f3d61d7438b
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-07-29 23:35:58 +0200

    petite corections

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../assets/Pages/Home";
 import About from "../assets/Pages/About";
 import Error from "../assets/Pages/Error";
@@ -8,7 +8,10 @@ import Accommodation from "../assets/Pages/Accommodation";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} /> {/* Route pour la page d'accueil */}
+      {/* Rediriger vers la page d'accueil */}
+      <Route path="/" element={<Navigate to="/Home" />} />
+
+      <Route path="/home" element={<Home />} /> {/* Route pour la page d'accueil */}
       <Route path="/about" element={<About />} /> {/* Route pour la page "About" */}
       <Route path="/error" element={<Error />} /> {/* Route pour la page d'erreur */}
       <Route exact path="/accommodation/:cardId" element={<Accommodation />} /> {/* Route pour la page de l'hébergement avec l'ID de la carte spécifié */}
