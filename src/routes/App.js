abe2bee5876cf816744131480241b361582a3a78
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-08-01 17:56:15 +0200

    index element Home

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../assets/Pages/Home";
 import About from "../assets/Pages/About";
 import Error from "../assets/Pages/Error";
@@ -9,8 +9,7 @@ function App() {
   return (
     <Routes>
       {/* Rediriger vers la page d'accueil */}
-      <Route path="/home" index element={<Home />} />
-      <Route path="/home" element={<Home />} /> {/* Route pour la page d'accueil */}
+      <Route index element={<Home />} /> {/* Route pour la page d'accueil */}
       <Route path="/about" element={<About />} /> {/* Route pour la page "About" */}
       <Route path="/error" element={<Error />} /> {/* Route pour la page d'erreur */}
       <Route exact path="/accommodation/:cardId" element={<Accommodation />} /> {/* Route pour la page de l'hébergement avec l'ID de la carte spécifié */}
