c4493cfc9cbd0d3c9b95d6efd1c7f30145f6e6f6
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-08-14 21:04:17 +0200

    correction * to home

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,25 +1,28 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../assets/Pages/Home";
-import About from "../assets/Pages/About";
-import Error from "../assets/Pages/Error";
-import Accommodation from "../assets/Pages/Accommodation";
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from '../assets/Pages/Home'
+import About from '../assets/Pages/About'
+import Error from '../assets/Pages/Error'
+import Accommodation from '../assets/Pages/Accommodation'
 
 function App() {
   return (
     <Routes>
-      {/* Rediriger vers la page d'accueil */}
-      <Route index element={<Home />} /> {/* Route pour la page d'accueil */}
-      <Route path="/about" element={<About />} /> {/* Route pour la page "About" */}
-      <Route path="/error" element={<Error />} /> {/* Route pour la page d'erreur */}
-      <Route exact path="/accommodation/:cardId" element={<Accommodation />} /> {/* Route pour la page de l'hébergement avec l'ID de la carte spécifié */}
-
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />{' '}
+      {/* Route pour la page "About" */}
+      <Route path="/error" element={<Error />} />{' '}
+      {/* Route pour la page d'erreur */}
+      <Route
+        exact
+        path="/accommodation/:cardId"
+        element={<Accommodation />}
+      />{' '}
+      {/* Route pour la page de l'hébergement avec l'ID de la carte spécifié */}
       {/* Route par défaut pour toutes les adresses non spécifiées */}
-      <Route path="*" element={<Error />} />
+      <Route path="*" element={<Home />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
-
-
+export default App
