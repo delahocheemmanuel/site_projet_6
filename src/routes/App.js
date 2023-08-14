f2067ce64ae52c0a16be9ce4154066777246ec4b
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-08-14 21:38:29 +0200

    correction * to hom2e

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../assets/Pages/Home'
 import About from '../assets/Pages/About'
-import Error from '../assets/Pages/Error'
+
 import Accommodation from '../assets/Pages/Accommodation'
 
 function App() {
@@ -11,15 +11,11 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />{' '}
       {/* Route pour la page "About" */}
-      <Route path="/error" element={<Error />} />{' '}
-      {/* Route pour la page d'erreur */}
       <Route
         exact
         path="/accommodation/:cardId"
         element={<Accommodation />}
-      />{' '}
-      {/* Route pour la page de l'hébergement avec l'ID de la carte spécifié */}
-      {/* Route par défaut pour toutes les adresses non spécifiées */}
+      /> 
       <Route path="*" element={<Home />} />
     </Routes>
   )
